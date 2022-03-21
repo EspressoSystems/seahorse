c96cffc7d3342cfb17f6f5fdb6349218912237af
jbearer <jbearer@users.noreply.github.com>
2022-03-21 20:53:35 +0000

    deploy: db28ad949e2c520934a281b4c2d34fe679b32224

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["seahorse"] = {"name":"","files":["accounts.rs","asset_library.rs","cli.rs","encryption.rs","events.rs","hd.rs","io.rs","lib.rs","loader.rs","persistence.rs","reader.rs","secret.rs","txn_builder.rs"]};
+sourcesIndex["seahorse"] = {"name":"","files":["accounts.rs","asset_library.rs","cli.rs","encryption.rs","events.rs","hd.rs","io.rs","key_scan.rs","lib.rs","loader.rs","persistence.rs","reader.rs","secret.rs","txn_builder.rs"]};
 createSourceSidebar();
