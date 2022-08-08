f423293bcfecb99dda950c5ad71af6639dbb7f92
bfish713 <bfish713@users.noreply.github.com>
2022-08-08 19:02:08 +0000

    deploy: 6fd565de9ea73309f8dbf89883c6e36dc98bdc59

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["seahorse"] = {"name":"","dirs":[{"name":"loader","files":["create.rs","interactive.rs","login.rs","recovery.rs"]}],"files":["accounts.rs","assets.rs","cli.rs","encryption.rs","events.rs","hd.rs","io.rs","key_scan.rs","key_value_store.rs","lib.rs","loader.rs","persistence.rs","reader.rs","secret.rs","sparse_merkle_tree.rs","state.rs","transactions.rs","txn_builder.rs"]};
+sourcesIndex["seahorse"] = {"name":"","dirs":[{"name":"loader","files":["create.rs","interactive.rs","login.rs","recovery.rs"]}],"files":["accounts.rs","assets.rs","cli.rs","encryption.rs","events.rs","hd.rs","io.rs","key_scan.rs","key_value_store.rs","lib.rs","loader.rs","persistence.rs","reader.rs","records.rs","secret.rs","sparse_merkle_tree.rs","state.rs","transactions.rs","txn_builder.rs"]};
 createSourceSidebar();
